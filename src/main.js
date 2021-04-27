fcb6798fa20fc4a7e4a7f2a4aa0e4411757f1ccd
943985375 <m15802667613@163.com>
2021-04-27 18:00:24 +0800

    基础功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入Element
+import ElemntUI from 'element-ui'
+// 引入Element 的主题文件
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式文件
+import './styles/index.scss'
+
+// 将Element 注册为Vue插件
+Vue.use(ElemntUI)
+
 Vue.config.productionTip = false
 
 new Vue({
